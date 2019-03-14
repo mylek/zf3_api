b233929416546b2b5fd42252f98840952d2f82c3
k.gwozdziowski@ideo.pl <k.gwozdziowski@ideo.pl>
2019-03-14 16:08:55 +0100

    test

--- app/module/Application/src/Lib/Page/Test.php
+++ app/module/Application/src/Lib/Page/Test.php
@@ -1,11 +1,17 @@
 <?php
 
 namespace Application\Lib\Page;
+use Zend\Http\Client;
 
 class Test extends Page
 {
 	public function parsedPage()
 	{
-		return true;
+		$client = new Client('http://ideo.pl', array(
+			'maxredirects' => 0,
+			'timeout'      => 30
+		));
+		$response = $client->send();
+		var_dump($response->getBody());
 	}
 }
